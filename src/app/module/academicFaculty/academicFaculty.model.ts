9d7ddb8d81823a4c02907b64f125be7e784a89a9
promahbubul <mahbublalam500@gmail.com>
2025-07-04 10:53:26 +0600

    [UPDATE] Create academic department modal

--- src/app/module/academicFaculty/academicFaculty.model.ts
+++ src/app/module/academicFaculty/academicFaculty.model.ts
@@ -1,14 +1,20 @@
 import { model, Schema } from 'mongoose';
 import { TAcademicFaculty } from './academicFaculty.interface';
 
-const academicFacultySchema = new Schema<TAcademicFaculty>({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const academicFacultySchema = new Schema<TAcademicFaculty>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-});
+  { timestamps: true },
+);
 
-const AcademicFaculty = model('AcademicFaculty', academicFacultySchema);
+const AcademicFaculty = model<TAcademicFaculty>(
+  'AcademicFaculty',
+  academicFacultySchema,
+);
 
 export default AcademicFaculty;
